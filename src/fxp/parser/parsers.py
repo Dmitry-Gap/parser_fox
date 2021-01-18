d31071f5932bc7e7801f918b22c1193c7dfa102c
Dmitry <dimitry.happ@mail.ru>
2021-01-18 19:20:21 +0300

    news-parser

--- src/fxp/parser/parsers.py
+++ src/fxp/parser/parsers.py
@@ -1,9 +1,12 @@
 import os
+import re
 import json
 import pickle
 import requests
 from bs4 import BeautifulSoup as BS
 from abc import ABC, abstractmethod
+from datetime import datetime, timezone
+from concurrent.futures import ThreadPoolExecutor
 
 from fxp.parser import DEFAULT_USER_AGENT, HOST, PREVIEW_URL, BASE_DIR
 
@@ -25,6 +28,7 @@ class BaseMeta(metaclass=_Base):
 class BaseParser(BaseMeta):
     __metaclass__ = ABC
 
+    _user_agent = DEFAULT_USER_AGENT
     def __init__(self, user_agent: str = None):
         self._user_agent = user_agent if user_agent is not None else DEFAULT_USER_AGENT
 
@@ -38,10 +42,10 @@ class BaseParser(BaseMeta):
                 "User-Agent": self._user_agent,
             },
         )
-        if hasattr(self, "page"):
-            if not response.url.endswith(str(self.page)):
-                if self.page != 1:
-                    raise ValueError("Page is very big!")
+        # if hasattr(self, "page"):
+        #     if not response.url.endswith(str(self.page)):
+        #         if self.page != 1:
+        #             raise ValueError("Page is very big!")
         if response.status_code == 200:
             return BS(response.text, features="html5lib")
         raise ValueError("Response not 200")
@@ -99,8 +103,7 @@ class Preview(BaseParser):
             self.__cursor += 1
 
     def __getitem__(self, index):
-        try:
-            return self.__links[index]
+        return self.__links[index]
         # except ValueError as error:
         #     print(error)
 
@@ -114,29 +117,67 @@ class Preview(BaseParser):
 
 
 class NewsParser(BaseParser):
-    def __init__(self, url):
-        self._url = url
+    def __init__(self, user_agent:str = None):
+        self.date_pattern = re.compile(r"[A-Za-z ,]+[\dAMP:]+")
+        super().__init__(user_agent)
         self.news = {}
 
-    def get_news(self):
+    def __call__(self, url):
         try:
-            html = self._get_page(self._url)
+            html = self._get_page(url)
         except ValueError as error:
             print(error)
         else:
             box = html.find("section", attrs={"id": "leftColumn"})
             if box is not None:
-                self.news["head"] = box.find("h1", atrrs={"class": "arcicleHeader"}).text
-                box_data = box.find("div", attrs={"class":"contentSectionDetails"})
-                news_date = box_data.find("span").text
+                self.news["head"] = box.find("h1", attrs={"class": "articleHeader"}).text
+                box_data = box.find("div", attrs=
+                {"class":"contentSectionDetails"})
+                self.news["date"] = (
+                    datetime.strptime(
+                    "".join(self.date_pattern.findall(box_data.find("span").text)), "%b %d, %Y %H:%M%p"
+                    )
+                    .replace(tzinfo=timezone.utc)
+                    .timestamp()
+                )
+                article = box.find("div", attrs={"class": "articlePage"})
+                text_block = article.find_all("p")
+                self.news["text"] = "\n".join([p.text for p in text_block]).strip()
+                self.news["img_link"] = article.find("img").attrs["src"]
+        self.save_to_json(
+            datetime.fromtimestamp(self.news["date"]).strftime("%Y/%m/%d/%H_%M")
+        )
+    
+    def save_to_json(self, name):
+        path = os.path.join(BASE_DIR, name + ".json")
+        dirs = os.path.split(path)[:-1]
+        try:
+            os.makedirs(os.path.join(*dirs))
+        except Exception as error:
+            print(error)
+        json.dump(self.news, open(path, "w"), ensure_ascii=False)
+
+
 
 if __name__ == "__main__":
-    parser = Preview(page=2)
-    parser.get_links()
-    print(parser._Preview__links)
-    parser.save_to_json("tmp_links_5")
-    parser.save_to_file("tmp_links_5")
-    for link in parser:
-        print(link)
-    a = parser[0:3]
-    print(a)
+    pool = ThreadPoolExecutor()
+    for page in range(1, 2):
+        links = Preview(page=page)
+        links.get_links()
+        news = NewsParser()
+        start = datetime.now()
+        news_from_page = pool.map(news, links) 
+        list(news_from_page)
+        print(datetime.now()-start) 
+    
+    
+    # news(parser[0])
+    
+    
+    # print(parser._Preview__links)
+    # parser.save_to_json("tmp_links_5")
+    # parser.save_to_file("tmp_links_5")
+    # for link in parser:
+    #     print(link)
+    # a = parser[0:3]
+    # print(a)
